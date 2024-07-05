8ac201ed8dea6f2939162a1419956b4d1b17676e
Marketing-3CPlus <charles.jose@3cplusnow.com>
2024-07-05 16:17:40 -0300

    add bootstrap

--- src/js/validations.js
+++ src/js/validations.js
@@ -18,32 +18,32 @@ validation
       errorMessage: "Razão Social é obrigatória",
     },
   ])
-  // .addField("#email", [
-  //   {
-  //     rule: "required",
-  //     errorMessage: "Email é obrigatório",
-  //   },
-  //   {
-  //     rule: "email",
-  //     errorMessage: "Email inválido",
-  //   },
-  // ])
-  // .addField("#cnpj", [
-  //   {
-  //     rule: "required",
-  //     errorMessage: "CNPJ é obrigatório",
-  //   },
-  //   {
-  //     rule: "minLength",
-  //     value: 18,
-  //     errorMessage: "CNPJ deve ter 14 dígitos",
-  //   },
-  //   {
-  //     rule: "maxLength",
-  //     value: 18,
-  //     errorMessage: "CNPJ deve ter 14 dígitos",
-  //   },
-  // ])
+  .addField("#email", [
+    {
+      rule: "required",
+      errorMessage: "Email é obrigatório",
+    },
+    {
+      rule: "email",
+      errorMessage: "Email inválido",
+    },
+  ])
+  .addField("#cnpj", [
+    {
+      rule: "required",
+      errorMessage: "CNPJ é obrigatório",
+    },
+    {
+      rule: "minLength",
+      value: 18,
+      errorMessage: "CNPJ deve ter 14 dígitos",
+    },
+    {
+      rule: "maxLength",
+      value: 18,
+      errorMessage: "CNPJ deve ter 14 dígitos",
+    },
+  ])
   .addField("#phone", [
     {
       rule: "required",
@@ -58,55 +58,55 @@ validation
       errorMessage: "Digite um telefone válido",
     },
   ])
-  // .addField("#cep", [
-  //   {
-  //     rule: "required",
-  //     errorMessage: "CEP é obrigatório",
-  //   },
-  // {
-  //   rule: "number",
-  //   errorMessage: "CEP deve conter apenas números",
-  // },
-  // ])
-  // .addField("#address", [
-  //   {
-  //     rule: "required",
-  //     errorMessage: "Endereço é obrigatório",
-  //   },
-  // ])
-  // .addField("#number", [
-  //   {
-  //     rule: "required",
-  //     errorMessage: "Número é obrigatório",
-  //   },
-  // ])
-  // .addField("#neighborhood", [
-  //   {
-  //     rule: "required",
-  //     errorMessage: "Bairro é obrigatório",
-  //   },
-  // ])
-  // .addField("#city", [
-  //   {
-  //     rule: "required",
-  //     errorMessage: "Cidade é obrigatória",
-  //   },
-  // ])
-  // .addField("#state", [
-  //   {
-  //     rule: "required",
-  //     errorMessage: "Estado é obrigatório",
-  //   },
-  //   {
-  //     rule: "maxLength",
-  //     value: 2,
-  //     errorMessage: "Estado deve ter 2 letras",
-  //   },
-  //   {
-  //     rule: "required",
-  //     errorMessage: "Estado deve estar em letras maiúsculas",
-  //   },
-  // ])
+  .addField("#cep", [
+    {
+      rule: "required",
+      errorMessage: "CEP é obrigatório",
+    },
+    {
+      rule: "number",
+      errorMessage: "CEP deve conter apenas números",
+    },
+  ])
+  .addField("#address", [
+    {
+      rule: "required",
+      errorMessage: "Endereço é obrigatório",
+    },
+  ])
+  .addField("#number", [
+    {
+      rule: "required",
+      errorMessage: "Número é obrigatório",
+    },
+  ])
+  .addField("#neighborhood", [
+    {
+      rule: "required",
+      errorMessage: "Bairro é obrigatório",
+    },
+  ])
+  .addField("#city", [
+    {
+      rule: "required",
+      errorMessage: "Cidade é obrigatória",
+    },
+  ])
+  .addField("#state", [
+    {
+      rule: "required",
+      errorMessage: "Estado é obrigatório",
+    },
+    {
+      rule: "maxLength",
+      value: 2,
+      errorMessage: "Estado deve ter 2 letras",
+    },
+    {
+      rule: "required",
+      errorMessage: "Estado deve estar em letras maiúsculas",
+    },
+  ])
   .addField("#connections", [
     {
       rule: "required",
@@ -122,16 +122,16 @@ validation
       errorMessage: "Número de Conexões deve ser maior que 2",
     },
   ])
-  // .addField("#users", [
-  //   {
-  //     rule: "required",
-  //     errorMessage: "Número de Usuários é obrigatório",
-  //   },
-  //   {
-  //     rule: "number",
-  //     errorMessage: "Número de Usuários deve ser um número",
-  //   },
-  // ]);
+  .addField("#users", [
+    {
+      rule: "required",
+      errorMessage: "Número de Usuários é obrigatório",
+    },
+    {
+      rule: "number",
+      errorMessage: "Número de Usuários deve ser um número",
+    },
+  ])
   .onSuccess((event) => {
     submitForm();
   });
